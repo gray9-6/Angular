07b86a7708cfa5b52700c00c1bca56f32ce1383a
Ajay Kumar <aky22896@gmail.com>
2023-12-24 14:02:06 +0530

    learned about selectords

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+//  selector: '[app-servers]',    // select the attribute like a string
+//  selector: '.app-servers ',    // select by class
+  selector: 'app-servers',      // like an element, normally used this
   template: `<app-server></app-server>
               <app-server></app-server>`,
   styleUrls: ['./servers.component.css']

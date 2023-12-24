54585bc384f141773e09d2c9cff62aae8cb32da1
Ajay Kumar <aky22896@gmail.com>
2023-12-24 18:39:08 +0530

    learned about data binding using string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,10 +4,17 @@ import { Component, OnInit } from '@angular/core';
 //  selector: '[app-servers]',    // select the attribute like a string
 //  selector: '.app-servers ',    // select by class
   selector: 'app-servers',      // like an element, normally used this
-  template: `<app-server></app-server>
-              <app-server></app-server>`,
+
+
+//  template: `<app-server></app-server>                    // in-line template
+//              <app-server></app-server>`,
+
+
+
+
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent implements OnInit {
 
   constructor() { }

faf6918d303ae3f6969210c026067d4f6b77690e
Ajay Kumar <aky22896@gmail.com>
2023-12-24 19:02:09 +0530

    learned about data binding using property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-//  selector: '[app-servers]',    // select the attribute like a string
-//  selector: '.app-servers ',    // select by class
+
   selector: 'app-servers',      // like an element, normally used this
-
-
-//  template: `<app-server></app-server>                    // in-line template
-//              <app-server></app-server>`,
-
-
-
-
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit(): void {
   }
